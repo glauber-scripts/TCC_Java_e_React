fa21484e0dea25bd505e7b338a88716217db4bc1
glauber-scripts <glaubervinicius44@gmail.com>
2026-06-25 18:59:16 -0300

    Delete backend/SistemaLivrariaMaven directory

--- backend/SistemaLivrariaMaven/src/main/java/com/example/LivroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LivroRepository extends JpaRepository<Livro, Integer> {
-    // Todos os métodos básicos de CRUD já nascem prontos aqui
-}
