cbaee2ef2e5e38e402a91037e679789a1acf691e
glauber-scripts <glaubervinicius44@gmail.com>
2026-06-25 18:59:26 -0300

    Delete frontend/Livraria directory

--- frontend/Livraria/src/Page1.jsx
+++
@@ -1,4 +0,0 @@
-function Header(){
-  return <h1 class="text-aligner">Olá mundo!</h1>
-}
-export default Header
--- frontend/Livraria/src/Titulo.jsx
+++
@@ -1,5 +0,0 @@
-
-function Titulo(){
-  return (<h1>Olá mundo!</h1>)
-}
-export default Titulo
